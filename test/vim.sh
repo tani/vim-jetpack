ead5b60be66dffaaf64ba3604deac11537800e4a
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-02-18 20:20:32 +0900

    Add null.vim

--- test/vim.sh
+++ test/vim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 TESTDIR=$(dirname $(realpath $0))
 rm -rf "$TESTDIR/pack"
-vim -s /dev/null -u "$TESTDIR/vimrc" -c 'JetpackSync' -c 'quitall'
+vim -s "$TESTDIR/null.vim" -u "$TESTDIR/vimrc" -c 'JetpackSync' -c 'quitall'
 vim -u "$TESTDIR/vimrc" -s "$TESTDIR/test.vim" -c 'quitall'
