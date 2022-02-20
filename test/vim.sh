0e248f220f6dffdb0efd0f05c1333c0f808ea3f1
m@docs.casa <taniguchi@jaist.ac.jp>
2022-02-20 20:10:45 +0900

    Show test results

--- test/vim.sh
+++ test/vim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 TESTDIR=$(dirname $(realpath $0))
 rm -rf "$TESTDIR/pack"
-vim -s "$TESTDIR/null.vim" -u "$TESTDIR/vimrc" -c 'JetpackSync' -c 'quitall'
-vim -u "$TESTDIR/vimrc" -s "$TESTDIR/test.vim" -c 'quitall'
+vim -u "$TESTDIR/vimrc" -c 'JetpackSync' -c 'quitall'
+vim -u "$TESTDIR/vimrc" -c "source $TESTDIR/test.vim" -c 'quitall'
